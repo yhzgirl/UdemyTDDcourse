3c6d6df40e70aaa620b10c2132b7de23af07d6b1
Michelle <michelle@Laptop.(none)>
2013-10-11 18:04:37 +0100

    add class String Calculator

---
+++ lib/string_calc.rb
@@ -0,0 +1,8 @@
+class StringCalculator
+
+	def add(numbers)
+		return 0 if numbers == ""
+		return 1 if numbers == "1"
+	end
+
+end
--- spec/string_calc_spec.rb
+++ spec/string_calc_spec.rb
@@ -1,9 +1,20 @@
 require_relative "../lib/string_calc"
+
 describe "String Calculator" do
-	describe "#adding" do
+
+	describe "#add" do
+
+		context "when there is one number" do
+			it "returns that number" do
+			 result = StringCalculator.new.add("1")
+				expect(result).to eq(1)
+			end
+		end
+
 		context "when there are 0 numbers" do
-			it "returns 0" do
-				res
+			it "returns zero" do
+				result = StringCalculator.new.add("")
+				expect(result).to eq(0)
 			end
 		end
 	end
