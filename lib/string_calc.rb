f1febd795fe3a9f60c816facd64b9b20cf63ca15
Michelle <michelle@Laptop.(none)>
2013-11-28 11:59:02 +0000

    stage one of kata done

--- lib/string_calc.rb
+++ lib/string_calc.rb
@@ -1,8 +1,19 @@
 class StringCalculator
 
-	def add(numbers)
-		return 0 if numbers == ""
-		return 1 if numbers == "1"
-	end
+  def add(numbers)
+    return default_result if numbers == ""
+    return result(numbers) if numbers
+  end
 
+  private
+
+  def result(numbers)
+    striped_string = numbers.scan(/\d/)
+    num_arry = striped_string.map { |n| n.to_i }
+    num_arry.inject{|sum,x| sum + x }
+  end
+
+  def default_result
+    0
+  end
 end
--- spec/string_calc_spec.rb
+++ spec/string_calc_spec.rb
@@ -2,19 +2,27 @@ require_relative "../lib/string_calc"
 
 describe "String Calculator" do
 
-	describe "#add" do
+	let(:result) { StringCalculator.new }
 
+	describe "#add" do
 		context "when there is one number" do
 			it "returns that number" do
-			 result = StringCalculator.new.add("1")
-				expect(result).to eq(1)
+				expect(result.add("1")).to eq(1)
+			end
+			it 'returns that number when it is another number' do
+					expect(result.add('2')).to eq(2)
 			end
 		end
 
 		context "when there are 0 numbers" do
 			it "returns zero" do
-				result = StringCalculator.new.add("")
-				expect(result).to eq(0)
+				expect(result.add('')).to eq(0)
+			end
+		end
+
+		context 'when there are 2 numbers' do
+			it 'returns the sum of those numbers' do
+				expect(result.add('1, 2')).to eq(3)
 			end
 		end
 	end
